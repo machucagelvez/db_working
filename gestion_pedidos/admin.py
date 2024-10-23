64883fed76a2690f22c344947d00025851551dfd
Fabián Machuca <machucagelvez@gmail.com>
2024-10-23 11:43:52 -0500

    contro panel

--- gestion_pedidos/admin.py
+++ gestion_pedidos/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from gestion_pedidos.models import Articulo, Cliente, Pedido
 
 # Register your models here.
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    # Permite definir los campos que se ven en el panel de administración:
+    list_display = ("nombre", "direccion", "email", "telefono")
+
+    # Permite definir los campos que se pueden buscar:
+    search_fields = ("nombre", "direccion")
+
+
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "seccion")
+    # Permite filtrar los artículos por sección:
+    list_filter = ("seccion",)  # La coma se agrega para indicar que es una tupla.
+
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ("numero", "fecha", "entregado")
+    list_filter = ("fecha",)
+    date_hierarchy = "fecha"
+
+
+admin.site.register(Articulo, ArticuloAdmin)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Pedido, PedidosAdmin)
--- gestion_pedidos/models.py
+++ gestion_pedidos/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 
 
-class Clientes(models.Model):
+class Cliente(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
-    telefono = models.CharField(max_length=10)
+    email = models.EmailField(blank=True, null=True)
+    # verbose_name es el nombre que aparece en el panel de administrador
+    telefono = models.CharField(max_length=10, verbose_name="Teléfono")
+
+    # __str__ permite modificar como se muestra cada objeto en el panel de administrador
+    def __str__(self):
+        return self.nombre
 
 
-class Articulos(models.Model):
+class Articulo(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
 
 
-class Pedidos(models.Model):
+class Pedido(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()
     entregado = models.BooleanField()

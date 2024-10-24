36f15340b6ac6c6eec9ab5002b1d2c0a29b36a62
Fabián Machuca <machucagelvez@gmail.com>
2024-10-24 09:26:53 -0500

    forms

--- gestion_pedidos/views.py
+++ gestion_pedidos/views.py
@@ -1,3 +1,39 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from gestion_pedidos.models import Articulo
+
 # Create your views here.
+
+
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+
+def buscar(request):
+    if request.GET["prd"]:
+        # mensaje = "Artículo buscado: %r" % request.GET["prd"]
+        producto = request.GET["prd"]
+
+        if len(producto) > 20:
+            mensaje = "Texto demasiado extenso"
+        else:
+            # articulos = Articulo.objects.filter(nombre=producto)
+            # nombre__icontains = like en sql:
+            articulos = Articulo.objects.filter(nombre__icontains=producto)
+
+            return render(
+                request,
+                "resultados_busqueda.html",
+                {"articulos": articulos, "query": producto},
+            )
+    else:
+        mensaje = "Debes escribir algo para buscar"
+
+    return HttpResponse(mensaje)
+
+
+def contacto(request):
+    if request.method == "POST":
+        return render(request, "gracias.html")
+    return render(request, "contacto.html")
